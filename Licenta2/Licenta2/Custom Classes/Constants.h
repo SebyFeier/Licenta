75ba79d0018e42b8872957c5df357c055f7611d4
Seby Feier <feier.sebastian@gmail.com>
2015-04-06 19:37:44 +0300

    changed server

--- Licenta2/Licenta2/Custom Classes/Constants.h
+++ Licenta2/Licenta2/Custom Classes/Constants.h
@@ -9,10 +9,10 @@
 #ifndef Licenta_Constants_h
 #define Licenta_Constants_h
 
-//#define kServerUrl @"http://sebytheboy.mzzhost.com"
+#define kServerUrl @"http://sebytheboy.mzzhost.com/Licenta"
 //#define kServerUrl @"http://192.168.0.103/~SebyFeier"
 //#define kServerUrl @"http://192.168.0.110/~SebyFeier"
-#define kServerUrl @"http://localhost/~SebyFeier/WebService"
+//#define kServerUrl @"http://localhost/~SebyFeier/WebService"
 
 typedef enum {
     DeviceRequest = 1,
